e006aff29f548a35557321b96d4c569b71ae0add
jbdevelop <jbernardorj@gmail.com>
2020-07-07 19:19:16 -0300

    MVP Build

---
+++ challenge-pokegama/build/precache-manifest.ae561266f795dac6b7523b8eb867faa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85942236974375d25209f445cd8fb01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eea0a8b95136a070f806",
+    "url": "/static/css/main.3c175acb.chunk.css"
+  },
+  {
+    "revision": "04ec6145d167a54986c1",
+    "url": "/static/js/2.4c02fa0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c02fa0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea0a8b95136a070f806",
+    "url": "/static/js/main.da932612.chunk.js"
+  },
+  {
+    "revision": "c1ec1313fa96dbe0070e",
+    "url": "/static/js/runtime-main.921fd582.js"
+  }
+]);
